3a3f79154d67a55007e69dd9de23ce236942ee38
RufaeelAsad <rufaeelasad@gmail.com>
2022-10-31 22:19:01 +0500

    Almost Complete Quiz App

---
+++ lib/answer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class Answer extends StatelessWidget {
+
+  final void Function()? selectIndiaction;
+
+  final String? answerText;
+
+  Answer(this.selectIndiaction, this.answerText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        child: Text(answerText!),
+        onPressed: selectIndiaction,
+      ),
+    );
+  }
+}
